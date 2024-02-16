eb78177c64a80b21897a8e2a5b7e97e397dcc4d6
Heather Taylor <76629753+taylorfullstack@users.noreply.github.com>
2024-02-16 12:49:56 +0100

    update jest config

--- front/jest.config.js
+++ front/jest.config.js
@@ -3,7 +3,7 @@ module.exports = {
     '@core/(.*)': '<rootDir>/src/app/core/$1',
   },
   preset: 'jest-preset-angular',
-  setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
+  setupFilesAfterEnv: ['<rootDir>/setup-jest.ts', '<rootDir>/node_modules/@hirez_io/observer-spy/dist/setup-auto-unsubscribe.js'],
   bail: false,
   verbose: false,
   collectCoverage: false,
