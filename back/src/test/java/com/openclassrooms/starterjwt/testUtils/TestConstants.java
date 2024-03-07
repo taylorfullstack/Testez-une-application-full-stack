26e78941596e3968ad7a0c37c7e3bf1ddb289931
Heather Taylor <76629753+taylorfullstack@users.noreply.github.com>
2024-03-07 16:26:37 +0100

    add constants for tests

---
+++ back/src/test/java/com/openclassrooms/starterjwt/testUtils/TestConstants.java
@@ -0,0 +1,26 @@
+package com.openclassrooms.starterjwt.testUtils;
+
+public class TestConstants {
+    // Test Database constants
+    public static final String TEST_SCRIPT = "classpath:test_script.sql";
+
+    // User constants
+    public static final String TEST_USER_EMAIL = "user@studio.com";
+    public static final String TEST_USER_FIRST_NAME = "TestUserFirstName";
+    public static final String TEST_USER_LAST_NAME = "TestUserLastName";
+    public static final String TEST_USER_PASSWORD = "TestUserPassword";
+    public static final String TEST_USER_ENCODED_PASSWORD = "TestUserEncodedPassword";
+    public static final String TEST_JWT_TOKEN = "TestJWTToken";
+
+    //Admin constants
+    public static final String TEST_ADMIN_EMAIL = "yoga@studio.com";
+    public static final String TEST_ADMIN_ROLE = "ADMIN";
+
+    // Teacher constants
+    public static final String TEST_TEACHER_FIRST_NAME = "TestTeacherFirstName";
+    public static final String TEST_TEACHER_LAST_NAME = "TestTeacherLastName";
+
+    // Session constants
+    public static final String TEST_SESSION_NAME = "TestSessionName";
+    public static final String TEST_SESSION_DESCRIPTION = "TestSessionDescription";
+}
