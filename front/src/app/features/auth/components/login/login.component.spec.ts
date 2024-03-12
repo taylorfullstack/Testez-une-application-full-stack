18febcd905d82d71c25257a838fbccf236406e3d
Heather Taylor <76629753+taylorfullstack@users.noreply.github.com>
2024-03-12 20:46:24 +0100

    add integration tests for features/auth

--- front/src/app/features/auth/components/login/login.component.spec.ts
+++ front/src/app/features/auth/components/login/login.component.spec.ts
@@ -1,4 +1,3 @@
-import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatCardModule } from '@angular/material/card';
@@ -7,23 +6,37 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterTestingModule } from '@angular/router/testing';
-import { expect } from '@jest/globals';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 import { SessionService } from 'src/app/services/session.service';
-
+import { expect } from '@jest/globals';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { LoginComponent } from './login.component';
+import { mockSessionInformation, mockLoginRequest} from "../../../../../test-constants";
+import { NgZone } from "@angular/core";
 
-describe('LoginComponent', () => {
+describe('LoginComponent integration', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
+  let httpMock: HttpTestingController;
+  let router: Router;
+  let ngZone: NgZone;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [LoginComponent],
-      providers: [SessionService],
+      providers: [
+        AuthService,
+        SessionService
+      ],
       imports: [
-        RouterTestingModule,
+        RouterTestingModule.withRoutes(
+          [
+            { path: 'sessions', redirectTo: '/sessions'}
+          ]
+        ),
         BrowserAnimationsModule,
-        HttpClientModule,
+        HttpClientTestingModule,
         MatCardModule,
         MatIconModule,
         MatFormFieldModule,
@@ -33,10 +46,78 @@ describe('LoginComponent', () => {
       .compileComponents();
     fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
+    router = TestBed.inject(Router);
+    ngZone = TestBed.inject(NgZone);
+    httpMock = TestBed.inject(HttpTestingController);
+    jest.spyOn(router, 'navigate');
+
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  afterEach(() => {
+    httpMock.verify();
+  });
+
+  it('should create the login component', () => {
     expect(component).toBeTruthy();
+    expect(component.hide).toBe(true);
+    expect(component.onError).toBe(false);
+    expect(component.form.dirty).toBe(false);
+    const button = fixture.debugElement.nativeElement.querySelector('button[type="submit"]');
+    expect(button.disabled).toBe(true);
+  });
+
+
+  it('should call AuthService.login() with correct parameters and navigate to /sessions when login attempt is successful', () => {
+    component.form.setValue(mockLoginRequest);
+    fixture.detectChanges();
+
+    expect(component.form.valid).toBe(true);
+    const button = fixture.debugElement.nativeElement.querySelector('button[type="submit"]');
+    expect(button.disabled).toBe(false);
+
+    ngZone.run(() => {
+      component.submit();
+
+      const req = httpMock.expectOne('api/auth/login');
+      expect(req.request.body).toEqual(mockLoginRequest);
+      req.flush(mockSessionInformation);
+
+      expect(router.navigate).toHaveBeenCalledWith(['/sessions']);
+    });
+  });
+
+  it('should set onError to true on failed login attempt', () => {
+    component.form.setValue(mockLoginRequest);
+    fixture.detectChanges();
+
+    ngZone.run(() => {
+      component.submit();
+
+      const req = httpMock.expectOne('api/auth/login');
+      req.flush('Error', { status: 500, statusText: 'Server Error' });
+
+      fixture.detectChanges();
+
+      expect(component.onError).toBe(true);
+      const errorMessage = fixture.debugElement.nativeElement.querySelector('.error');
+      expect(errorMessage.textContent).toContain('An error occurred');
+    });
   });
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- front/src/app/features/auth/components/register/register.component.spec.ts
+++ front/src/app/features/auth/components/register/register.component.spec.ts
@@ -1,26 +1,40 @@
-import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { expect } from '@jest/globals';
-
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { RegisterComponent } from './register.component';
+import { AuthService } from "../../services/auth.service";
+import { RouterTestingModule } from "@angular/router/testing";
+import { Router } from '@angular/router';
+import { NgZone } from '@angular/core';
+import { mockRegisterRequest, mockSessionInformation } from "../../../../../test-constants";
+import { LoginComponent } from "../login/login.component";
 
 describe('RegisterComponent', () => {
   let component: RegisterComponent;
   let fixture: ComponentFixture<RegisterComponent>;
+  let httpMock: HttpTestingController;
+  let router: Router;
+  let ngZone: NgZone;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [RegisterComponent],
+      providers: [
+        AuthService,
+      ],
       imports: [
+        RouterTestingModule.withRoutes([
+          { path: "login", component: LoginComponent}
+        ]),
         BrowserAnimationsModule,
-        HttpClientModule,
-        ReactiveFormsModule,  
+        HttpClientTestingModule,
+        ReactiveFormsModule,
         MatCardModule,
         MatFormFieldModule,
         MatIconModule,
@@ -31,10 +45,58 @@ describe('RegisterComponent', () => {
 
     fixture = TestBed.createComponent(RegisterComponent);
     component = fixture.componentInstance;
+    httpMock = TestBed.inject(HttpTestingController);
+    router = TestBed.inject(Router);
+    ngZone = TestBed.inject(NgZone);
+    jest.spyOn(router, 'navigate');
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  afterEach(() => {
+    httpMock.verify();
+  });
+
+  it('should create the register component', () => {
     expect(component).toBeTruthy();
+    expect(component.onError).toBe(false);
+    expect(component.form.dirty).toBe(false);
+    const button = fixture.debugElement.nativeElement.querySelector('button[type="submit"]');
+    expect(button.disabled).toBe(true);
+  });
+
+  it('should call AuthService.register() with correct parameters and navigate to /login when registration attempt is successful', () => {
+    component.form.setValue(mockRegisterRequest);
+    fixture.detectChanges();
+
+    expect(component.form.valid).toBe(true);
+    const button = fixture.debugElement.nativeElement.querySelector('button[type="submit"]');
+    expect(button.disabled).toBe(false);
+
+    ngZone.run(() => {
+        component.submit();
+
+        const req = httpMock.expectOne('api/auth/register');
+        expect(req.request.body).toEqual(mockRegisterRequest);
+        req.flush(mockSessionInformation);
+        expect(router.navigate).toHaveBeenCalledWith(['/login']);
+    });
+  });
+
+
+  it('should set onError to true on failed registration attempt', () => {
+    component.form.setValue(mockRegisterRequest);
+    fixture.detectChanges();
+    ngZone.run(() => {
+      component.submit();
+
+      const req = httpMock.expectOne('api/auth/register');
+      req.flush('Error', { status: 500, statusText: 'Server Error' });
+
+      fixture.detectChanges();
+
+      expect(component.onError).toBe(true);
+      const errorMessage = fixture.debugElement.nativeElement.querySelector('.error');
+      expect(errorMessage.textContent).toContain('An error occurred');
+    });
   });
 });
