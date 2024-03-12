2e18241372fae341f0106cc4a96d44baeaf01171
Heather Taylor <76629753+taylorfullstack@users.noreply.github.com>
2024-03-12 18:52:44 +0100

    update app component test

--- front/src/app/app.component.spec.ts
+++ front/src/app/app.component.spec.ts
@@ -8,6 +8,8 @@ import { expect } from '@jest/globals';
 import { HttpClientTestingModule } from "@angular/common/http/testing";
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { NgZone } from "@angular/core";
+import { By } from '@angular/platform-browser';
+import { of } from "rxjs";
 
 describe('AppComponent', () => {
   let component: AppComponent;
@@ -28,22 +30,73 @@ describe('AppComponent', () => {
     sessionService = TestBed.inject(SessionService);
     router = TestBed.inject(Router);
     ngZone = TestBed.inject(NgZone);
+    fixture.detectChanges();
   });
 
   it('should create the app with $isLogged as false', () => {
     expect(component).toBeTruthy();
+    const loginLink = fixture.debugElement.query(By.css('[routerLink="login"]'));
+    const registerLink = fixture.debugElement.query(By.css('[routerLink="register"]'));
+    const meLink = fixture.debugElement.query(By.css('[routerLink="me"]'));
+
     component.$isLogged().subscribe(isLogged => {
       expect(isLogged).toBe(false);
+      expect(loginLink).toBeTruthy();
+      expect(registerLink).toBeTruthy();
+      expect(meLink).toBeFalsy();
     });
   });
 
-  it('should call logOut method of SessionService and navigate to the root route', () => {
+  it('should create the app with $isLogged as true', () => {
+    jest.spyOn(sessionService, '$isLogged').mockReturnValue(of(true));
+
+    fixture.detectChanges();
+
+    const loginLink = fixture.debugElement.query(By.css('[routerLink="login"]'));
+    const registerLink = fixture.debugElement.query(By.css('[routerLink="register"]'));
+    const meLink = fixture.debugElement.query(By.css('[routerLink="me"]'));
+
+    component.$isLogged().subscribe(isLogged => {
+      expect(isLogged).toBe(true);
+      expect(loginLink).toBeFalsy();
+      expect(registerLink).toBeFalsy();
+      expect(meLink).toBeTruthy();
+    });
+  });
+
+  it('should call logOut method of SessionService and navigate to the root route', async () => {
     const navigateSpy = jest.spyOn(router, 'navigate');
+    const isLoggedSpy = jest.spyOn(sessionService, '$isLogged').mockReturnValue(of(true));
+
+    fixture.detectChanges(); // update view with new isLogged value
+
+    let meLink = fixture.debugElement.query(By.css('[routerLink="me"]'));
+    let logoutLink = fixture.debugElement.queryAll(By.css('.link'))
+      .find(de => de.nativeElement.textContent === 'Logout');
+
+    component.$isLogged().subscribe(isLogged => {
+      expect(isLogged).toBe(true);
+    });
+
+    expect(meLink).toBeTruthy();
+    expect(logoutLink).toBeTruthy();
+
     jest.spyOn(sessionService, 'logOut');
+
     ngZone.run(() => {
       component.logout();
-      expect(sessionService.logOut).toHaveBeenCalled();
-      expect(navigateSpy).toHaveBeenCalledWith(['']);
+      isLoggedSpy.mockRestore();
+      fixture.detectChanges();
     });
+
+    let loginLink = fixture.debugElement.query(By.css('[routerLink="login"]'));
+    let meLinkAfterLogout = fixture.debugElement.query(By.css('[routerLink="me"]'));
+    expect(sessionService.logOut).toHaveBeenCalled();
+    component.$isLogged().subscribe(isLogged => {
+      expect(isLogged).toBe(false);
+    });
+    expect(navigateSpy).toHaveBeenCalledWith(['']);
+    expect(loginLink).toBeTruthy();
+    expect(meLinkAfterLogout).toBeFalsy();
   });
 });
